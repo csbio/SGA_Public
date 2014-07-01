4d3583a6fa1a6997a1a3d1e06ae77ff418e5a9da
Benjamin VanderSluis <bvander@cs.umn.edu>
2014-07-01 14:16:07 -0500

    re-added fill_square_array
    insubstantial changes to clustergram

--- postprocess/generate_fg_clustergram.m
+++ postprocess/generate_fg_clustergram.m
@@ -3,31 +3,22 @@ function[] = generate_fg_clustergram(sga, name)
 % produces a CLUST and CHROM version
 
 
-	%fid = fopen('/project/csbio/lab_share/SGA/refdata/StrainID-Allele_map_140107.csv', 'r');
-	fid = fopen('/project/csbio/benjamin/Data/StrainID-Allele_map_140116.csv', 'r');
+	fid = fopen('~/Research/Data/YeastGeneMap/strain2orf2common2allele_140521.txt', 'r');
 
-	A = textscan(fid, '%s%s');
+	A = textscan(fid, '%s%s%s%s', 'Delimiter', '\t', 'ReturnOnError', false);
 	fclose(fid);
 
 	m = java.util.HashMap();
 	for i=1:1:length(A{1})
-		m.put(java.lang.String(A{1}{i}), java.lang.String(A{2}{i}));
+		m.put(java.lang.String(A{1}{i}), java.lang.String(A{4}{i}));
 	end
 
 	allele = sga.Cannon.Orf;
 	for i=1:length(allele)
-		ix = strfind(allele{i}, '_ts');
-		if isempty(ix)
-			allele{i} = sga.Cannon.Common{i};
-			continue
+		a = m.get(allele{i});
+		if ~isempty(a)
+			allele{i} = a;
 		end
-
-		a = m.get(allele{i}(ix+1:end));
-		if isempty(a)
-			continue
-		end
-
-		allele{i} = a;
 	end
 	sga.Cannon.allele = allele;
 
@@ -42,22 +33,21 @@ function[] = generate_fg_clustergram(sga, name)
 	qix = ChromSort(qorf);
 	aix = ChromSort(aorf);
 
-	print_pcl_file(EPS(qix, aix), qallele(qix), qorf(qix), aorf(aix), [name '_CHROM.pcl']);
-	%print_pcl_file(EPS(qix, aix), qallele(qix), qorf(qix), aallele(aix), [name '_CHROM.pcl']);
+	 print_pcl_file(EPS(qix, aix), qallele(qix), qorf(qix), aorf(aix), [name '_CHROM.pcl']);
+	% print_pcl_file(EPS(qix, aix), qallele(qix), qorf(qix), aallele(aix), [name '_CHROM.pcl']);
 
 
 	% Zero insignif - data
-	fprintf('Zeroing insignificant data\n');
-	EPS(isnan(EPS)) = 0;
-	EPS(EPS > -0.08 & EPS < 0.08) = 0;
-	EPS(PVL > 0.05) = 0;
-	%fprintf('NOT  Zeroing insignificant data\n');
+	% fprintf('Zeroing insignificant data\n');
+	% EPS(isnan(EPS)) = 0;
+	% EPS(EPS > -0.08 & EPS < 0.08) = 0;
+	% EPS(PVL > 0.05) = 0;
+	fprintf('NOT  Zeroing insignificant data\n');
 	
 	MyCluster(EPS,...
 				 sga.Cannon.Orf(sga.Cannon.isQuery), ...
 				 sga.Cannon.allele(sga.Cannon.isQuery), ...
 				 sga.Cannon.allele(sga.Cannon.isArray), [name '_CLUST']);
-				 %sga.Cannon.Orf(sga.Cannon.isArray), [name '_CLUST']);
 			 
 
 
