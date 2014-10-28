d0ad186dba387ed2629d93c15e81da6241e5a95a
Benjamin VanderSluis <bvander@cs.umn.edu>
2014-10-28 11:57:51 -0500

    compute_sgascore_function wrapper implemented
    a function which takes in a struct holding input parameters.
    Cleans up after it self ()-style, and in good for running jobs
    in series.

---
+++ compute_sgascore_function.m
@@ -0,0 +1,23 @@
+function [] = compute_sgascore_function(parameter_struct)
+%function [] = compute_sgascore_function(parameter_struct)
+% for when you really need a function instead of a script
+
+    % unpack the parameters
+    parameters = fieldnames(parameter_struct);
+    for i = 1:length(parameters)
+        eval(sprintf('%s = parameter_struct.%s', parameters{i}));
+    end
+
+    % clear up the unpack (just in case)
+    clear parameters parameter_struct i
+
+    keyboard % check state
+
+    % turn on the debugger to gain all the advantages of a script!
+    dbstop if error
+
+    % run the script
+    compute_sgascore
+
+end
+
