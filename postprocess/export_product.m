0965d0526608dd99c3bc7bd62c4cae20fdd3b22f
Benjamin VanderSluis <bvander@cs.umn.edu>
2015-03-18 08:32:28 -0500

    qvar fix export_product
    changes output file, and skips some products

--- postprocess/export_product.m
+++ postprocess/export_product.m
@@ -1,7 +1,7 @@
 function[sga] = export_product(sga_inputfile, sga_outputfile, smfitnessfile, ...
-              linkagefile, coord_file, layout_file, equiv_file, wild_type, border_strain, skip_filter)
+              linkagefile, coord_file, layout_file, equiv_file, wild_type, border_strain)
 %function[] = export_product(sga_inputfile, sga_outputfile, smfitnessfile, ...
-%                  linkagefile, coord_file, layout_file, wild_type, border_strain, skip_filter)
+%                  linkagefile, coord_file, layout_file, wild_type, border_strain)
 % post processing pipeline...
 % Loads the datafile (it is stupid to write the file, then load it, but at least it keeps things modular)
 % green-block
@@ -14,54 +14,55 @@ function[sga] = export_product(sga_inputfile, sga_outputfile, smfitnessfile, ...
 % ie, it is extensionless
 	
 
-	sga_raw = load_sga_epsilon_from_scorefile([sga_outputfile '.txt'], [sga_outputfile '.orf']);
+	% sga_raw = load_sga_epsilon_from_scorefile([sga_outputfile '.txt'], [sga_outputfile '.orf']);
+    sga_outputfile = [sga_outputfile '_qvarfix'];
+	keyboard % load and rename to skip this step.
+
 
 	% layout file expects format (plate row col orf) [384] 16x24
-
-	keyboard
 	sga = filter_green_blocks_around_linkage(sga_raw, linkagefile, coord_file, layout_file, wild_type, border_strain);
 
 	% this will do cobatch filter and AB BA disagreement filtereing in either case
 	% but will not do any fitness depedant filtering unless 'false'
 	[sga, fitness_struct]  = filter_interactions(sga, smfitnessfile, sga_inputfile, equiv_file);
 
-
-	% ------------------------ clustergrams 
-	
 	dirname = split_by_delimiter('/', sga_outputfile);
 	basename= split_by_delimiter('_', dirname{end});
-	
-	clus_dirname = [join_by_delimiter(dirname(1:end-1), '/') '/clustergrams/'];
-	clus_basename = basename;
-	clus_basename{1} = 'clustergram';
-	clus_basename = join_by_delimiter(clus_basename, '_');
-	system(['mkdir -p ' clus_dirname]);
-	generate_fg_clustergram(sga, [clus_dirname clus_basename]);
+	int_dirname = [join_by_delimiter(dirname(1:end-1), '/') '/interactions/'];
+	system(['mkdir -p ' int_dirname]);
+
+	% ------------------------ clustergrams 
+	% clus_dirname = [join_by_delimiter(dirname(1:end-1), '/') '/clustergrams/'];
+	% system(['mkdir -p ' clus_dirname]);
+	% clus_basename = basename;
+	% clus_basename{1} = 'clustergram';
+	% clus_basename = join_by_delimiter(clus_basename, '_');
+	% generate_fg_clustergram(sga, [clus_dirname clus_basename]);
 
 
 	% ------------------------ interactions / profiles
 
-	int_dirname = [join_by_delimiter(dirname(1:end-1), '/') '/interactions/'];
-	int_basename{1} = 'release';
-	system(['mkdir -p ' int_dirname]);
-	prof_basename = basename;
-	prof_basename{1} = 'profile';
-	prof_basename = join_by_delimiter(prof_basename, '_');
-	print_profile_data(sga, [int_dirname '/' prof_basename '.txt']);
+	% int_basename{1} = 'release';
+	% prof_basename = basename;
+	% prof_basename{1} = 'profile';
+	% prof_basename = join_by_delimiter(prof_basename, '_');
+	% print_profile_data(sga, [int_dirname '/' prof_basename '.txt']);
 
 
 	% ------------------------ processed matfiles
 		fields = split_by_delimiter('_', basename);
-		project = fields{2};
+		% project = fields{2};
+		project = 'sgaQvar';
+		keyboard
 		array   = fields{3};
 		temp    = fields{4};
 		construct = join_by_delimiter({project, array, temp}, '_');
 
 	eval(sprintf('%s = sga;', construct)); % rename struct
-	eval(sprintf('save %s%s.mat %s ', int_dirname, construct, construct)); % save to mat
+	eval(sprintf('save %s%s.mat %s  -v7.3', int_dirname, construct, construct)); % save to mat
 
 	eval(sprintf('%s_raw = sga_raw;', construct)); % save a pre-filter version as well
-	eval(sprintf('save %s%s_raw.mat %s_raw', int_dirname, construct, construct)); % save to mat
+	eval(sprintf('save %s%s_raw.mat %s_raw -v7.3', int_dirname, construct, construct)); % save to mat
 
 end
 
