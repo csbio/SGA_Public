80525ed0a3a99fae8b2c3b69c31aec70cd49614d
Benjamin VanderSluis <ben@bjv.us>
2016-07-07 11:47:34 -0400

    removed SplitOrfs, too triple specific
    it remains as a subfunction, so sort_by_chr still
    supports triple data

--- util/sort_by_chr.m
+++ util/sort_by_chr.m
@@ -46,3 +46,35 @@ function[sorted_list, sorted_ix] = sort_by_chr(orf_array)
 
    sorted_list = orf_array_save(sorted_ix);
 end
+
+% these functions are helpers to handle doulbe mutant control queries
+function[a, b] = SplitOrfs(cellarr, delim)
+	% splits on first +, returns pieces
+
+	if ~exist('delim', 'var')
+		delim = '+';
+	end
+
+	% support a single string also
+	if isstr(cellarr)
+		[a, b] = single_split(cellarr, delim);
+	else
+		a = cell(size(cellarr));
+		b = cell(size(cellarr));
+		for i=1:length(cellarr)
+			[a{i}, b{i}] = single_split(cellarr{i}, delim);
+		end
+	end
+end
+
+function[a, b] = single_split(string, delim)
+	ix = strfind(string, delim);
+	if(isempty(ix))
+		a = string;
+		b = '';
+	else
+		a = string(1:ix-1);
+		b = string(ix+1:end);
+	end
+end
+
