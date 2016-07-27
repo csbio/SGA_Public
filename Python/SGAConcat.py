8fd92708d8d977c0b276edabb00f1d026fe3379c
Benjamin VanderSluis <ben@bjv.us>
2016-07-27 15:43:27 -0400

    fixes #22
    auto-decode fileinput stream if filename ends in .gz

--- Python/SGAConcat.py
+++ Python/SGAConcat.py
@@ -6,7 +6,7 @@ import os
 import fileinput
 
 def help():
-    HELP_TEXT = """ 
+   HELP_TEXT = """ 
 #############################################################################
 # SGAConcat.py
 # 
@@ -14,9 +14,9 @@ def help():
 # concatenates the other data.
 #
 #   plate numbers are incremented past the max plate number in SGAfile
-#   batch ids     are incremented past the max batch number in SGAfile
-# 	        Each of these are first assigned unique IDs (from 1)
-#          to prevent large gaps in batch ids
+#   batch ids    are incremented past the max batch number in SGAfile
+# 	      Each of these are first assigned unique IDs (from 1)
+#        to prevent large gaps in batch ids
 #
 # Author: Benjamin VanderSluis (bvander@cs.umn.edu)
 # Revision: January 26, 2015
@@ -25,8 +25,8 @@ def help():
 # SGAConcat.py SGAfile.gz OtherData.gz > outputfile
 #  
 # INPUTS:
-#    SGAfile.gz     gzipped raw sgadata file
-#    OtherData.gz  gzipped raw sga data
+#   SGAfile.gz    gzipped raw sgadata file
+#   OtherData.gz  gzipped raw sga data
 #
 # SGA_file and OtherData may be gzipped 
 #
@@ -34,41 +34,43 @@ def help():
 #
 #############################################################################
 """
-    print(HELP_TEXT, file=sys.stderr)
-    return
+   print(HELP_TEXT, file=sys.stderr)
+   return
 
 ################ MAIN FUNCTION
 
 if sys.argv.count('-h') + sys.argv.count('-help') + sys.argv.count('--help') > 0:
-    help()
-    sys.exit()
+   help()
+   sys.exit()
 
 if len(sys.argv) != 3:
-    print('Wrong number of arguments, try "-help"', file=sys.stderr)
-    sys.exit()
+   print('Wrong number of arguments, try "-help"', file=sys.stderr)
+   sys.exit()
 
 SGAfile = sys.argv[1]
 OtherData = sys.argv[2]
 
 fid_1 = fileinput.hook_compressed(SGAfile, 'r')
 fid_2 = fileinput.hook_compressed(OtherData, 'r')
+
 max_plate = 0
 max_batch = 0
 for line in fid_1:
-    # if file was actually zipped fileinput reads bytes(bug)
-    #line = line.decode('utf-8').strip()
-    line = line.strip()
+   if SGAfile[-3:] == '.gz':
+      line = line.decode('utf-8').strip()
+   else:
+      line = line.strip()
 
-    split_line = line.split('\t')
-    plate = int(split_line[4])
-    batch = int(split_line[5])
-    if plate > max_plate:
-        max_plate = plate
+   split_line = line.split('\t')
+   plate = int(split_line[4])
+   batch = int(split_line[5])
+   if plate > max_plate:
+      max_plate = plate
 
-    if batch > max_batch:
-        max_batch = batch
+   if batch > max_batch:
+      max_batch = batch
 
-    print(line)
+   print(line)
 
 fid_1.close()
 
@@ -82,23 +84,27 @@ other_plates = {}
 plates_seen = 0
 
 for line in fid_2:
-    line = line.decode('utf-8').strip()
-    #line = line.strip()
-    line = line.split('\t')
-    plate = line[4]
-    batch = line[5]
-    
-    # assign unique ids to other_file batches and plates on the fly
-    if plate not in other_plates:
-        plates_seen = plates_seen+1
-        other_plates[plate] = plates_seen
-    if batch not in other_batches:
-        batches_seen = batches_seen+1
-        other_batches[batch] = batches_seen
 
-    line[4] = str(max_plate +  other_plates[plate])
-    line[5] = str(max_batch + other_batches[batch])
-    line = '\t'.join(line)
-    print(line)
+   if OtherData[-3:] == '.gz':
+      line = line.decode('utf-8').strip()
+   else:
+      line = line.strip()
+
+   line = line.split('\t')
+   plate = line[4]
+   batch = line[5]
+   
+   # assign unique ids to other_file batches and plates on the fly
+   if plate not in other_plates:
+      plates_seen = plates_seen+1
+      other_plates[plate] = plates_seen
+   if batch not in other_batches:
+      batches_seen = batches_seen+1
+      other_batches[batch] = batches_seen
+
+   line[4] = str(max_plate +  other_plates[plate])
+   line[5] = str(max_batch + other_batches[batch])
+   line = '\t'.join(line)
+   print(line)
 
 fid_2.close()
